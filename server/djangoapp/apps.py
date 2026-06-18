a1419aaf629310830a930a12265284c3afb1ef0d
wbrothersCuba <yanet.wilmar@gmail.com>
2026-06-18 00:09:59 -0400

    Beging implementation of adding  Dynamic Pages

--- server/djangoapp/apps.py
+++ server/djangoapp/apps.py
@@ -1,5 +1,5 @@
 from django.apps import AppConfig
 
-
 class DjangoappConfig(AppConfig):
     name = 'djangoapp'
+
--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -103,11 +103,16 @@ def get_dealerships(request, state="All"):
 
 
 # Create a `get_dealer_reviews` view to render the reviews of a dealer
-def get_dealer_details(request, dealer_id):
+def get_dealer_reviews(request, dealer_id):
+    # if dealer id has been provided
     if(dealer_id):
-        endpoint = "/fetchDealer/"+str(dealer_id)
-        dealership = get_request(endpoint)
-        return JsonResponse({"status":200,"dealer":dealership})
+        endpoint = "/fetchReviews/dealer/"+str(dealer_id)
+        reviews = get_request(endpoint)
+        for review_detail in reviews:
+            response = analyze_review_sentiments(review_detail['review'])
+            print(response)
+            review_detail['sentiment'] = response['sentiment']
+        return JsonResponse({"status":200,"reviews":reviews})
     else:
         return JsonResponse({"status":400,"message":"Bad Request"})
 
