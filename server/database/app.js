ee23bdce82695d31385e0cd32ca4f5d72f550abe
wbrothersCuba <yanet.wilmar@gmail.com>
2026-06-21 13:52:07 -0400

    Fixing js lint errors line 36

--- server/database/app.js
+++ server/database/app.js
@@ -33,7 +33,7 @@ try {
 
 // Express route to home
 app.get('/', async (req, res) => {
-    res.send("Welcome to the Mongoose API")
+    res.send("Welcome to the Mongoose API");
 });
 
 // Express route to fetch all reviews
